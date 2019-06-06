9f87459917c6952a0fb69b0d54b91274649cab6b
Plalangan <P.lalangan@hotmail.com>
2019-06-05 20:28:31 -0700

    pokemon objects
    added 3 objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-alert('Hello world');
+var charmander = {
+  name: 'Charmander',
+  height: 5,
+  type: 'Fire',
+};
+
+var bulbasur = {
+  name: 'Bulbasur',
+  height: 3,
+  type: 'Grass'
+};
+
+var squirtle = {
+  name: 'Squirtle',
+  height: 4,
+  type: 'Water'
+};
+var repository = [charmander, bulbasur, squirtle];
+
+
+for (var i = 0; i < repository.length; i++) {
+  document.write('<p>Name:  ' + repository[i].name + '     ' + 'Height:' + repository[i].height + ' feet' +  '      ' + 'Type:' + repository[i].type   + '</p>');
+
+}
